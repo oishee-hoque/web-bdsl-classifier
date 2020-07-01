49dcdb141b66aaa938844a2fe58ef113c9a7355d
Oishee <mashrurmahee@gmail.com>
2020-07-02 05:52:49 +0600

    final

--- src/app.py
+++ src/app.py
@@ -114,6 +114,6 @@ if __name__ == '__main__':
     if "prepare" not in sys.argv:
         app.jinja_env.auto_reload = True
         app.config['TEMPLATES_AUTO_RELOAD'] = True
-        app.run(debug=False, host='0.0.0.0', port=port)
-        #app.run(debug=False, host='127.0.0.1', port=port)
+        #app.run(debug=False, host='0.0.0.0', port=port)
+        app.run(debug=False, host='127.0.0.1', port=port)
         # app.run(host='0.0.0.0', port=port)
